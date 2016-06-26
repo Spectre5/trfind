97a20569fd84142a2966fad5f462e74e710ac1b7
thatneat <jrussellcurtis@gmail.com>
2016-06-25 20:57:58 -0700

    Basic argparser for testing

--- trfind/find.py
+++ trfind/find.py
@@ -1,9 +1,28 @@
+import argparse
+import itertools
+import sys
+
 from trfind.finders import ALL_FINDERS
 from trfind.models import Peak
 
 
 def main():
-    peak = Peak('Mount Stuart', None, None)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('lat', type=float)
+    parser.add_argument('lon', type=float)
+    parser.add_argument(
+        'name',
+        nargs='+',
+        action='append',
+        help='Peak name to search for - multiple words will form one search term'
+    )
+    args = parser.parse_args()
+    peak_name = ' '.join(*args.name) or 'Mount Stuart'
+    
+    peak = Peak(peak_name, args.lat, args.lon)
+    print 'Finding trip reports for', peak
 
-    for finder in ALL_FINDERS:
-        print finder(peak)
+    all_trip_reports = list(itertools.chain(
+        *(finder(peak) for finder in ALL_FINDERS)
+    ))
+    print all_trip_reports
