2a9a91605df7cd34bed8b40b83db8c87584b44cf
Jaime <jaime.m.mccandless@gmail.com>
2016-06-25 23:20:29 -0700

    use get_all_trip_reports in app

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, jsonify, request
 
-from trfind.finders import peakbagger
+from trfind.find import get_all_trip_reports
 from trfind.models import Peak
 
 app = Flask(__name__)
@@ -12,10 +12,9 @@ def find():
     json = request.json
 
     peak = Peak(**json['data'])
-    trip_reports = peakbagger.find(peak)
-    return jsonify({'data': trip_reports})
+    trip_reports = get_all_trip_reports(peak)
+    return jsonify({'data': tuple(trip_report._asdict() for trip_report in trip_reports)})
 
 if __name__ == "__main__":
-    print 'running'
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
