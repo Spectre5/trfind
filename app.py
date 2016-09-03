1d425bbb33c82300eb299f45c976c384a7cf81f9
thatneat <jrussellcurtis@gmail.com>
2016-09-03 14:32:59 -0700

    try and be OK with non-"application/json" request content-type

--- app.py
+++ app.py
@@ -10,22 +10,24 @@ from trfind.weather import get_weather
 app = Flask(__name__)
 
 
+def _get_peak_from_request():
+    json = request.json or json.loads(request.data)
+    return Peak(**json['data'])
+
+
 @app.route('/find', methods=['POST'])
 @cross_origin('http://www.climbplan.com', 'localhost')
 def find():
-    json = request.json
-
-    peak = Peak(**json['data'])
+    peak = _get_peak_from_request()
     trip_reports = get_all_trip_reports(peak)
+
     return jsonify({'data': tuple(trip_report._asdict() for trip_report in trip_reports)})
 
 
 @app.route('/weather', methods=['POST'])
 @cross_origin('http://www.climbplan.com', 'localhost')
 def weather():
-    json = request.json
-
-    peak = Peak(**json['data'])
+    peak = _get_peak_from_request()
     weathers = get_weather(peak)
 
     return jsonify({'data': tuple(weather._asdict() for weather in weathers)})
