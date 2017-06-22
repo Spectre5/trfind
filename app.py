690af9d2669918ae32267e69d725a844e5156f28
thatneat <thatneat@gmail.com>
2017-06-22 14:15:24 -0700

    don't 500 if the right keys aren't there for Peak

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from flask_cors import cross_origin
 
 from trfind.find import get_all_trip_reports
@@ -10,14 +10,17 @@ from trfind.weather import get_weather
 app = Flask(__name__)
 
 
-def _get_peak_from_request():
-    return Peak(**request.json['data'])
+def _get_peak_from_request_or_400():
+    try:
+        return Peak(**request.json['data'])
+    except TypeError:
+        abort(400)
 
 
 @app.route('/find', methods=['POST'])
 @cross_origin('http://www.climbplan.com', 'localhost')
 def find():
-    peak = _get_peak_from_request()
+    peak = _get_peak_from_request_or_400()
     trip_reports = get_all_trip_reports(peak)
 
     return jsonify({'data': tuple(trip_report._asdict() for trip_report in trip_reports)})
@@ -26,7 +29,7 @@ def find():
 @app.route('/weather', methods=['POST'])
 @cross_origin('http://www.climbplan.com', 'localhost')
 def weather():
-    peak = _get_peak_from_request()
+    peak = _get_peak_from_request_or_400()
     weathers = get_weather(peak)
 
     return jsonify({'data': tuple(weather._asdict() for weather in weathers)})
